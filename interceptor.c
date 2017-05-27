78f71e565ef97c519a4e0fcbca5bade9244fba76
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-05-27 01:10:35 -0400

    i think i fixed the stuff

--- interceptor.c
+++ interceptor.c
@@ -408,65 +408,64 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 
 		case REQUEST_START_MONITORING:
 
-			// If invalid pid
-			if (pid < 0){
+			
+			// checking if pid is valid or not
+			if ((pid < 0) || ((pid != 0) && (pid_task(find_vpid(pid), PIDTYPE_PID) == NULL))) {
 				return -EINVAL;
 			}
-
-			if (table[syscall].monitored == 2){
-				if (((pid != 0) && (check_pid_monitored(syscall, pid) != 1)) ||
+			
+			// checking permissions
+			if (current_uid() != 0) {
+				if ((pid == 0) || (check_pid_from_list(current->pid, pid) != 0)) {
+					return -EPERM;
+				}
+			}
+			
+			
+			/* Check for if pid is already being monitored for blacklist where our flags are flipped */
+			if (table[syscall].monitored == 2) {
+				// if pid is not zero and its being monitored return EBUSY
+				// or if pid is zero and listcount is 0 (i.e. everything is being monitored) return EBUSY
+				if (((pid != 0) && (check_pid_monitored(syscall, pid) != 1)) || 
 					((table[syscall].listcount == 0) && (pid == 0))) {
-						return -EBUSY;
-					}
+					return -EBUSY;
+				}
 			} else {
-				if (table[syscall].monitored == 1){
+				// check if pid is in our normal (whitelist) implementation
+				if (check_pid_monitored(syscall, pid) == 1) {
 					return -EBUSY;
 				}
 			}
 			
-			if (table[syscall].intercepted == 0){
+			// check if syscall is intercepted
+			if (table[syscall].intercepted == 0) {
 				return -EINVAL;
 			}
-
-			if (pid == 0){
-				if (current_uid() != 0) {
-					return -EPERM;
-				}
-				spin_lock(&pidlist_lock);
+			
+			spin_lock(&pidlist_lock);
+			if (pid == 0) {
+				// set monitored to 2 to set up our blacklist implementation
 				destroy_list(syscall);
 				table[syscall].monitored = 2;
-				spin_unlock(&pidlist_lock);
-
 			} else {
-				if (pid_task(find_vpid(pid), PIDTYPE_PID) == NULL){
-					return -EINVAL;
-				}
-
-				// If its not root or pid not owned by parent process, return EPERM
-				if (current_uid() != 0 || check_pid_from_list(current->pid, pid) != 0){
-					return -EPERM;
-				}
-
-				// Normal implementation
-				if (table[syscall].monitored != 2){
-					spin_lock(&pidlist_lock);
-					if (add_pid_sysc(pid,syscall) == -ENOMEM){
+				// condition for our blacklist
+				if (table[syscall].monitored == 2) {
+					if (del_pid_sysc(pid, syscall) == -EINVAL) {
+						spin_unlock(&pidlist_lock);
+						return -EINVAL;
+					}
+					
+				} else {
+					// normal implementation and just monitor the pid
+					if (add_pid_sysc(pid, syscall) == -ENOMEM) {
 						spin_unlock(&pidlist_lock);
 						return -ENOMEM;
 					}
 					table[syscall].monitored = 1;
-					spin_unlock(&pidlist_lock);
-
-				// Blacklist implementation flips the flags
-				} else {
-					spin_lock(&pidlist_lock);
-					if(del_pid_sysc(pid, syscall) == -EINVAL){
-						spin_unlock(&pidlist_lock);
-						return -EINVAL;
-					}
-					spin_unlock(&pidlist_lock);
 				}
 			}
+			spin_unlock(&pidlist_lock);
+			
 			return 0;
 
 		case REQUEST_STOP_MONITORING:
@@ -475,17 +474,20 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			if (pid < 0){
 				return -EINVAL;
 			}
-
+			//check if not being intercepted or monitored
 			if (table[syscall].intercepted == 0 || table[syscall].monitored == 0){
-
 				return -EINVAL;
 			}
 			
+			//check if all pid's monitored
 			if (table[syscall].monitored == 2){
+				//if all monitored check our blacklist implementation flags
+				//where the flags are flipped
 				if (check_pid_monitored(syscall, pid) == 1){
 					return -EINVAL;
 				}
 			}else{
+				//else just check normal implementation and make sure pid is not zero
 				if((pid != 0) && (check_pid_monitored(syscall, pid) == 0)){
 					return -EINVAL;
 				}
