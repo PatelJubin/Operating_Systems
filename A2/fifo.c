479750c3f3ddbbed26f6d9a9a86ddbd26fe0f675
Mathun <mathun_m@hotmail.com>
2017-06-18 00:12:30 -0400

    Implemented fifo.

--- A2/fifo.c
+++ A2/fifo.c
@@ -12,13 +12,16 @@ extern int debug;
 
 extern struct frame *coremap;
 
+unsigned int fifo_front;
+
 /* Page to evict is chosen using the fifo algorithm.
  * Returns the page frame number (which is also the index in the coremap)
  * for the page that is to be evicted.
  */
 int fifo_evict() {
-	
-	return 0;
+	unsigned int coremap_idx = fifo_front; //page at the front of the queue is selected to evict
+	fifo_front = (fifo_front + 1) % memsize;
+	return coremap_idx;
 }
 
 /* This function is called on each access to a page to update any information
@@ -26,12 +29,12 @@ int fifo_evict() {
  * Input: The page table entry for the page that is being accessed.
  */
 void fifo_ref(pgtbl_entry_t *p) {
-
-	return;
+	return; //to-do
 }
 
 /* Initialize any data structures needed for this 
  * replacement algorithm 
  */
 void fifo_init() {
+	fifo_front = 0;
 }
