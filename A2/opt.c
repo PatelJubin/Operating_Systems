e5ab4475e6e5e76ba44c740a3cec1acbab449b3a
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-06-26 00:11:13 -0400

    done opt, need to test

--- A2/opt.c
+++ A2/opt.c
@@ -26,8 +26,41 @@ unsigned int count_idx;
  * for the page that is to be evicted.
  */
 int opt_evict() {
+	//initializing variables that will be needed
+	int evict_frame = 0;
+	int i = 0;
+	int j = count_idx;
+	int k = 0;
+	int longest_nextUsed = 0;
 	
-	return 0;
+	//loop through the virt_addr_list in memeory
+	//while populating our used_next array
+	for(i = 0; i < memsize; i++){
+		for (j = count_idx; j < tr_size ; j++){
+			//if we found the virtual address, we want
+			//to store the next used index.
+			if(virt_addr_list[j] == mem_addr_list[i]){
+				used_next[i] = j;
+				break;
+			}
+			//if we cant find the virtual address, return j
+			//which will be evicted.
+			if(j == tr_size){
+				return j;
+			}
+		}
+	}
+	
+	//swap out our furthest index and frame if our current frame's
+	//virtual address has a longer distance
+	for(k = 0; k < memsize; k++){
+		if (used_next[k] < longest_nextUsed){
+				evict_frame = k;
+				longest_nextUsed = used_next[k];
+		}
+	}
+	
+	return evict_frame;
 }
 
 /* This function is called on each access to a page to update any information
@@ -83,6 +116,8 @@ void opt_init() {
 			virt_addr_list[i] = vaddr;
 			i++;
 		}
-
+	}
 }
+
+
 
