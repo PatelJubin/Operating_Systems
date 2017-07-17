6b5fc2d976e94c5e18000edf9d7131cdabcacdae
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-07-16 23:19:10 -0400

    starter files for A3

---
+++ A3/ext2_ls.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <getopt.h>
+#include "ext2.h"
+#include <errno.h>
+#include <string.h>
+
+unsigned char *disk;
+
+int main(int argc, char *argv[]) {
+}
