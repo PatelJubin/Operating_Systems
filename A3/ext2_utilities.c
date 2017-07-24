5e92a1a38f7f29ce38510b314ac6eaeea9c3486a
Jubin Patel <pateljub@IITS-I406-09.utsc-labs.utoronto.ca>
2017-07-24 17:01:30 -0400

    helper code

---
+++ A3/ext2_utilities.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <getopt.h>
+#include "ext2.h"
+#include <errno.h>
+#include <string.h>
+
+
+
+struct ext2_inode *find_inode (char *path, unsigned char *disk){
+	ext2_group_desc *gp_desc = (struct ext2_group_desc *)disk + (EXT2_BLOCK_SIZE * EXT2_ROOT_INO);
+	ext2_inode *inode_table = (struct ext2_inode *) disk + (EXT2_BLOCK_SIZE * gp_desc->bg_inode_table);
+	ext2_inode *root_inode = &inode_table[EXT2_ROOT_INO];
+
+int block = 0;
+	ext2_dir_entry_2 *dir= (struct ext2_dir_entry_2 *)disk + (EXT2_BLOCK_SIZE * root_inode->i_block[block];
+char *rest;
+char *split_path = strtok_r(path, "/", &rest);
+
+while (split_path != NULL && block < 12 && root_inode->i_block[block]){
+
+	while( 
+		if (strcmp(split_path, dir->name, dir->name_len) == 0){
+			root_inode = &inode_table[dir->inode]; 
+			block = 0;
+		}
+		split_path = strtok_r(rest, "/", &rest);
+		block ++;
+	
+}
+
+
+
