b0b8f2048fd671792d307a5a34ebe78b8e1a60e3
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-07-28 15:16:31 -0400

    added in methods to check free spots in bitmap

--- A3/ext2_utilities.c
+++ A3/ext2_utilities.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -18,13 +17,13 @@ struct ext2_inode *find_inode (char *path, unsigned char *disk){
 	We get the root inode by getting the descriptor for the root, then getting the inode_table entry for
 	the root.
 	*/
-	ext2_group_desc *gp_desc = (struct ext2_group_desc *)disk + (EXT2_BLOCK_SIZE * EXT2_ROOT_INO);
-	ext2_inode *inode_table = (struct ext2_inode *) disk + (EXT2_BLOCK_SIZE * gp_desc->bg_inode_table);
-	ext2_inode *root_inode = &inode_table[EXT2_ROOT_INO - 1];
+	struct ext2_group_desc *gp_desc = (struct ext2_group_desc *)disk + (EXT2_BLOCK_SIZE * EXT2_ROOT_INO);
+	struct ext2_inode *inode_table = (struct ext2_inode *) disk + (EXT2_BLOCK_SIZE * gp_desc->bg_inode_table);
+	struct ext2_inode *root_inode = &inode_table[EXT2_ROOT_INO - 1];
 
 	//Initialize block and dir
 	int block = 0;
-	ext2_dir_entry_2 *dir;
+	struct ext2_dir_entry_2 *dir;
 
 	//We will split the path until we get to NULL
 	char *rest;
@@ -87,4 +86,48 @@ char *find_parent(char *path){
 	
 }
 
+unsigned int check_block_bitmap(unsigned char *disk){
+	struct ext2_super_block *sp_block = (struct ext2_super_block *) (disk + EXT2_BLOCK_SIZE);
+	struct ext2_group_desc *gp_desc = (struct ext2_group_desc *)(disk + (EXT2_ROOT_INO*EXT2_BLOCK_SIZE));
+	//make sure there are free blocks before looping through bitmap
+	if (sp_block->s_free_blocks_count > 0){
+		char *blk_bitmap = (char *)(disk + (gp_desc->bg_block_bitmap * EXT2_BLOCK_SIZE));
+		int i;
+		int j;
+		//loop through all of bitmap for free block
+		for (i=0;i<16;i++){
+			unsigned int bit = *(blk_bitmap + i);
+			for(j=0;j<8;j++){
+				//if the bit is not one then return that spot
+				if(((bit>>j) & 1) == 0){
+					return (i*8+j+1);
+				}
+			}
+		}
+	}
+	//else return -1 if no free block found
+	return -1;
+}
 
+unsigned int check_inode_bitmap(unsigned char *disk){
+	struct ext2_super_block *sp_block = (struct ext2_super_block *) (disk + EXT2_BLOCK_SIZE);
+	struct ext2_group_desc *gp_desc = (struct ext2_group_desc *)(disk + (EXT2_ROOT_INO*EXT2_BLOCK_SIZE));
+	//check number of free blocks
+	if (sp_block->s_free_blocks_count > 0){
+		char *inode_bitmap = (char *)(disk + (gp_desc->bg_inode_bitmap * EXT2_BLOCK_SIZE));
+		int i;
+		int j;
+		//loops through all of bitmap to find free spot
+		for (i=0;i<4;i++){
+			unsigned int bit = *(inode_bitmap + i);
+			for(j=0;j<8;j++){
+				//if the bit is not one then return that spot
+				if(((bit>>j) & 1) == 0){
+					return (i*8+j+1);
+				}
+			}
+		}
+	}
+	//else return -1 if no free inode found
+	return -1;
+}
