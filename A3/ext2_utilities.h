b28dbd7d003766dcbe55e709ff834e734bc0adaf
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-07-30 14:00:04 -0400

    more fixes

--- A3/ext2_utilities.h
+++ A3/ext2_utilities.h
@@ -5,4 +5,4 @@ unsigned int check_inode_bitmap(unsigned char *disk);
 void set_inode_bit(unsigned char *disk);
 void set_block_bit(unsigned char *disk);
 void unset_block_bit(unsigned int bmap_block, unsigned char *disk);
-void unset_inode_bit(unsigned int bmap_block, unsigned char *disk);
+void unset_inode_bit(unsigned int bmap_inode, unsigned char *disk);
