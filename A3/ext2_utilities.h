6ba0f1c584baec981d244eec58317debbc3a63f7
PatelJubin <jubin.patel@mail.utoronto.ca>
2017-07-30 13:52:42 -0400

    updated with new functions

--- A3/ext2_utilities.h
+++ A3/ext2_utilities.h
@@ -1,2 +1,8 @@
 struct ext2_inode *find_inode(char *path, unsigned char *disk);
 char *find_parent(char *path);
+unsigned int check_block_bitmap(unsigned char *disk);
+unsigned int check_inode_bitmap(unsigned char *disk);
+void set_inode_bit(unsigned char *disk);
+void set_block_bit(unsigned char *disk);
+void unset_block_bit(unsigned int bmap_block, unsigned char *disk);
+void unset_inode_bit(unsigned int bmap_block, unsigned char *disk);
